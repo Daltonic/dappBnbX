b68ca196d1ce394e817111afc3b41688eb405c13
Daltonic <darlingtongospel@gmail.com>
2023-11-10 12:53:34 +0100

    added deployment script

---
+++ scripts/deploy.js
@@ -0,0 +1,60 @@
+const { ethers } = require('hardhat')
+const fs = require('fs')
+
+async function deployContract() {
+  let contract
+  const taxPercent = 7
+  const securityFee = 5
+
+  try {
+    contract = await ethers.deployContract('DappBnb', [taxPercent, securityFee])
+    await contract.waitForDeployment()
+
+    console.log('Contracts deployed successfully.')
+    return contract
+  } catch (error) {
+    console.error('Error deploying contracts:', error)
+    throw error
+  }
+}
+
+async function saveContractAddress(contract) {
+  try {
+    const address = JSON.stringify(
+      {
+        dappBnbContract: contract.target,
+      },
+      null,
+      4
+    )
+
+    fs.writeFile('./contracts/contractAddress.json', address, 'utf8', (error) => {
+      if (error) {
+        console.error('Error saving contract address:', err)
+      } else {
+        console.log('Deployed contract address:', address)
+      }
+    })
+  } catch (error) {
+    console.error('Error saving contract address:', error)
+    throw error
+  }
+}
+
+async function main() {
+  let contract
+
+  try {
+    contract = await deployContract()
+    await saveContractAddress(contract)
+
+    console.log('Contract deployment completed successfully.')
+  } catch (error) {
+    console.error('Unhandled error:', error)
+  }
+}
+
+main().catch((error) => {
+  console.error('Unhandled error:', error)
+  process.exitCode = 1
+})
