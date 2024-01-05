b21c27a8840c927632aad68bf261721351d13350
Daltonic <darlingtongospel@gmail.com>
2024-01-05 14:57:02 +0100

    deployed to bitfinity

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,12 +6,13 @@ module.exports = {
     localhost: {
       url: 'http://127.0.0.1:8545',
     },
-    bitfinity: {
-      url: 'https://testnet.bitfinity.network',
-      accounts: [''],
-      chainId: 355113,
-      timeout: 120000,
-    },
+    // bitfinity: {
+    //   url: 'https://testnet.bitfinity.network',
+    //   accounts: [''],
+    //   chainId: 355113,
+    //   timeout: 120000,
+    //   gasPrice: 10 * 10**9,
+    // },
   },
   solidity: {
     version: '0.8.19',
